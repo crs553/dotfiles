38c81930a9ac31bf3ea92094f4371ae668d0fb68
CS <42813301+crs553@users.noreply.github.com>
2025-07-16 21:41:41 +0100

    feat: default font and size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,14 @@
 local wezterm = require 'wezterm'
 
-return {
-  color_scheme = "Catppuccin Mocha", -- Replace with your preferred flavor: Latte, Frappe, Macchiato, or Mocha
-}
+local config = {}
+
+config.color_scheme = "Catppuccin Mocha" -- Options: Latte, Frappe, Macchiato, or Mocha
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 12
+
+-- Miscellaneous settings
+config.window_close_confirmation = "NeverPrompt"
+config.automatically_reload_config = false
+config.audible_bell = "Disabled"
+
+return config
