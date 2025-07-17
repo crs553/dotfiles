fdbf311101625c206ff059d76d88e42cb173f526
CS <42813301+crs553@users.noreply.github.com>
2025-07-17 21:59:44 +0100

    fix: fix to deprecated function

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -108,23 +108,20 @@ return {
 
       })
 
-      --autoformat on save
+
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
           if not client then return end
-          if client.supports_method('textDocument/formatting') then
-            -- format the current buffer onsave
+          if client.server_capabilities.documentFormattingProvider then
             vim.api.nvim_create_autocmd('BufWritePre', {
               buffer = args.buf,
               callback = function()
                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
               end,
             })
-
-            -- Create a keymap for vim.lsp.buf.rename()
           end
-        end
+        end,
       })
     end,
   },
