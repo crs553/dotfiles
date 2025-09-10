c33692466ea98560c7898a29cfca781212e2fe02
crs553 <42813301+crs553@users.noreply.github.com>
2025-09-10 10:04:52 +0100

    fix: monitor changes per system

---
+++ hypr/conf/gen-monitor-config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+HOST=$(hostname)
+
+if [ "$HOST" = "pocket" ]; then
+    # GPD Pocket settings
+    hyprctl keyword monitor "eDP-1,preferred,auto,transform,90"
+    hyprctl keyword device:Goodix\ Capacitive\ TouchScreen:transform 90
+elif [ "$HOST" = "framework" ]; then
+    # Framework Laptop settings
+    hyprctl keyword monitor "eDP-1, preferred, auto, 1.333333"
+fi
