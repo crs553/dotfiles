946d938c19de7de0c982f264dc4812a867630fa2
CS <42813301+crs553@users.noreply.github.com>
2025-09-10 10:18:03 +0100

    fix: transform should now be correct

--- hypr/conf/gen-monitor-config.sh
+++ hypr/conf/gen-monitor-config.sh
@@ -3,7 +3,7 @@ HOST=$(hostname)
 
 if [ "$HOST" = "pocket" ]; then
     # GPD Pocket settings
-    hyprctl keyword monitor "eDP-1,preferred,auto,transform,90"
+    hyprctl keyword monitor "eDP-1,preferred,auto,1,transform,1"
     hyprctl keyword device:Goodix\ Capacitive\ TouchScreen:transform 90
 elif [ "$HOST" = "framework" ]; then
     # Framework Laptop settings
