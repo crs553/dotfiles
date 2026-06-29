bb02c41c6d6ef8c05303ba6a5bc04087718dee4b
CS <42813301+crs553@users.noreply.github.com>
2026-06-29 14:02:49 +0100

    fix(keyboard)

--- hypr/conf/keyboard.lua
+++ hypr/conf/keyboard.lua
@@ -1,38 +1,38 @@
 hl.config({
-  input = {
-    kb_layout = "gb",
-    kb_variant = "",
-    kb_model = "",
-    kb_options = "grp:alt_shift:toggle",
-    kb_rules = "",
-    follow_mouse = 1,
-    touchpad = {
-      natural_scroll = false,
-    },
-    sensitivity = 0,
-  },
+	input = {
+		kb_layout = "gb",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "grp:alt_shift_toggle",
+		kb_rules = "",
+		follow_mouse = 1,
+		touchpad = {
+			natural_scroll = false,
+		},
+		sensitivity = 0,
+	},
 })
 
 hl.device({
-  name = "glove80-keyboard",
-  kb_layout = "us",
-  kb_variant = "",
+	name = "glove80-keyboard",
+	kb_layout = "us",
+	kb_variant = "",
 })
 
 hl.device({
-  name = "moergo-glove80-left-keyboard",
-  kb_layout = "us",
-  kb_variant = "",
+	name = "moergo-glove80-left-keyboard",
+	kb_layout = "us",
+	kb_variant = "",
 })
 
 hl.device({
-  name = "bluetooth-5.1-keyboard",
-  kb_layout = "uk",
-  kb_variant = "",
+	name = "bluetooth-5.1-keyboard",
+	kb_layout = "uk",
+	kb_variant = "",
 })
 
 hl.device({
-  name = "sino-wealth-gaming-keyboard",
-  kb_layout = "us",
-  kb_variant = "",
+	name = "sino-wealth-gaming-keyboard",
+	kb_layout = "us",
+	kb_variant = "",
 })
